8ca20b85ad532e3ba6900272521b00a894860a18
Everton Paulino <evertonpaulino25@gmail.com>
2022-10-28 15:23:01 -0300

    Adicionar arquivos de projeto.

---
+++ Evolutionsoft2_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Evolutionsoft2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Razao = 1;
+            int Inicio = 1;
+
+            Console.WriteLine("Digite um número");
+            int Termino = int.Parse(Console.ReadLine());
+
+            int Soma = 0;
+
+            int Pa = Inicio;
+            Console.Write(" A soma dos números ");
+
+            for (int i = Inicio; i <= Termino; i += Razao)
+            {
+                Console.Write(Pa + ",");
+                Soma += Pa;
+                Pa += Razao;
+
+            }
+            Console.Write(" é igual " + Soma);
+            Console.ReadLine();
+        }
+    }
+}
